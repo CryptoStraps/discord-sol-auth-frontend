0cbefb4697993569f04ac3c7ad9f0649afb358f7
Alice <alize_in_cryptoland@protonmail.com>
2022-01-24 17:49:00 -0500

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -111,7 +111,7 @@ export default function Home() {
                           sendTransaction();
                         }}
                       >
-                        Connect Discord
+                        Send proof tx
                       </button>
                       <br />
                       {txid && (
@@ -136,7 +136,7 @@ export default function Home() {
                 <div className="card-body">
                   <a href="https://discord.com/api/oauth2/authorize?client_id=935204697030131712&redirect_uri=https%3A%2F%2Fsol-auth.vercel.app&response_type=code&scope=identify">
                     <button className="btn btn-primary">
-                      Make proof tx
+                      Connect Discord
                     </button>
                   </a>
                 </div>
