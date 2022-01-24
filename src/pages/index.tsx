dd960c56a3dfd7d8c988d518d8a2a84382ec7422
Alice <alice_in_cryptoland@pm.me>
2022-01-24 16:27:03 -0500

    F

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -140,9 +140,10 @@ export default function Home() {
                 </div>
               </div>
             </>
+            
           )}
         </div>
-        
+
       </main>
     </div>
   );
