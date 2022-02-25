8d7db365c2d470ce16210c5085091876d2c11d69
Alice <alize_in_cryptoland@protonmail.com>
2022-02-25 07:11:55 -0500

    woop woop

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,17 @@
-import {
-  useAnchorWallet,
-  useConnection,
-  useWallet,
-} from "@solana/wallet-adapter-react";
+import { useWallet } from "@solana/wallet-adapter-react";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
-import {
-  Transaction,
-  SystemProgram,
-  TransactionInstruction,
-  PublicKey,
-} from "@solana/web3.js";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import { useCallback, useEffect, useState } from "react";
 import { API_URL, DISCORD_API_URL } from "../constants";
 import nacl from "tweetnacl";
 import Image from "next/image";
-const sleep = (time = 1000) =>
-  new Promise((resolve, reject) => setTimeout(() => resolve(undefined), time));
 
 export default function Home() {
   const [user, setUser] = useState<any>();
   const { query } = useRouter();
   const { code } = query;
-  const { publicKey, signMessage, signTransaction } = useWallet();
-  const { connection } = useConnection();
-  const [txid, setTxId] = useState("");
+  const { publicKey, signMessage } = useWallet();
   const [txLoading, setTxLoading] = useState("");
   const [loggingIntoDiscord, setLogginIntoDiscord] = useState(false);
   const [error, setError] = useState(false);
@@ -78,7 +64,7 @@ export default function Home() {
         alert("Wallet doesnt support message signing");
         return;
       }
-      setTxLoading('loading');
+      setTxLoading("loading");
       const msg = user.id;
       const signed = await (window as any).solana.signMessage(
         new TextEncoder().encode(`${msg}`),
@@ -92,14 +78,16 @@ export default function Home() {
       );
       if (verified) {
         const sig = await fetch(
-          `${API_URL}/submit?signature=${signed.signature.toJSON().data}&pubkey=${publicKey.toBase58()}&discordId=${user.id}`
+          `${API_URL}/submit?signature=${
+            signed.signature.toJSON().data
+          }&pubkey=${publicKey.toBase58()}&discordId=${
+            user.id
+          }&discordHandle=${`${user.username}#${user.discriminator}`}`
         ).then((res) => res && res.json());
-        setTxId('foo');
-        setTxLoading('loaded');
-
+        setTxLoading("loaded");
       } else {
         setError(true);
-        setTxLoading('loaded');
+        setTxLoading("loaded");
       }
     }
   }, [publicKey, user]);
@@ -117,12 +105,22 @@ export default function Home() {
         className="flex fixed flex-col justify-center items-center"
         style={{ inset: 0 }}
       >
+        <div  style={{ position: "absolute", right: "2rem", top: "2rem", width: 200 }}>
+          <WalletMultiButton
+          />
+        </div>
         <div className="bg-black card">
           <div className="text-center card-body">
             <div className="mb-4 card-title">CryptoStraps Whitelist</div>
-            <div  className="m-3 shadow-lg">
-              <Image src="/android-chrome-192x192.png"  alt="Logo" width={192} height={192}/>
+            <div className="m-3 shadow-lg">
+              <Image
+                src="/android-chrome-192x192.png"
+                alt="Logo"
+                width={192}
+                height={192}
+              />
             </div>
+
             {!user && !code && !error && (
               <>
                 <h2 className="mb-3">1. Login to Discord</h2>
@@ -150,25 +148,21 @@ export default function Home() {
             )}
             {
               <>
-                {user && publicKey && txLoading === '' &&  (
+                {user && publicKey && txLoading === "" && (
                   <button
                     className="mt-2 btn btn-outline"
-                    onClick={() => {
-                      sendTransaction();
-                    }}
+                    onClick={() => sendTransaction()}
                   >
                     Sign Message
                   </button>
                 )}
-                {user && publicKey && txLoading === 'loading' && (
+                {user && publicKey && txLoading === "loading" && (
                   <>
                     <button disabled className="btn loading"></button>
                   </>
                 )}
-                {user && publicKey && txLoading === 'loaded' && (
-                  <>
-                    Success! We got your address
-                  </>
+                {user && publicKey && txLoading === "loaded" && (
+                  <>Success! We got your address</>
                 )}
               </>
             }
